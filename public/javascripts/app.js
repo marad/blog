5be141db7d1ab96ec347431088aa59325f28c8e5
Marcin Radoszewski <moriturius@gmail.com>
2015-02-20 21:06:35 +0000

    Improved how main menu works

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,2 +1,19 @@
 // Start highlighting code
 hljs.initHighlightingOnLoad();
+
+// Handle main menu
+YUI({
+    classNamePrefix: 'pure'
+}).use('gallery-sm-menu', function (Y) {
+
+    var horizontalMenu = new Y.Menu({
+        container         : '#menu-container',
+        sourceNode        : '#menu-items',
+        orientation       : 'horizontal',
+        hideOnOutsideClick: false,
+        hideOnClick       : false
+    });
+
+    horizontalMenu.render();
+    horizontalMenu.show();
+});
